fdbc68089a253e48cdd8568f5c16a48a8015c3da
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-12-29 23:34:36 -0800

    branchSums solution

---
+++ branchSums.py
@@ -0,0 +1,31 @@
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branchSums(root):
+    '''
+    Start traversing through the tree by starting at the root and making your way down starting with
+    the leftmost branch. Keep track of the sum as you go down each branch by appending the sum to an
+    array that tracks all sum values. Once you reach the end of the branch (None) move onto the next
+    branch starting from the root and repeating the process.
+    '''
+    sums = []
+    calculateBranchSums(root, 0, sums)
+    return sums
+
+
+def calculateBranchSums(node, runningSum, sums):
+    if node is None:
+        return
+
+    newRunningSum = runningSum + node.value
+    if node.left is None and node.right is None:
+        sums.append(newRunningSum)
+        return
+
+    calculateBranchSums(node.left, newRunningSum, sums)
+    calculateBranchSums(node.right, newRunningSum, sums)
