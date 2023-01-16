09f6d3211d1ae9eedc1c9f8fcbb3bc579ed42cde
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-01-15 18:20:11 -0800

    update findClosestValueInBST solution

--- findClosestValueInBST.py
+++ findClosestValueInBST.py
@@ -63,3 +63,47 @@ class BST:
         self.value = value
         self.left = None
         self.right = None
+
+
+# Same solution as second solution but with explanation/break down
+# Average: O(log(n)) time since we are technically removing either the entire left or
+# right side of the tree when we go down a branch | O(1) space
+
+# Worst: O(n) time | O(1) space
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, tree.value)
+
+
+def findClosestValueInBstHelper(tree, target, closest):
+    # use currentNode var to track current tree for each recursive call
+    currentNode = tree
+    # iterate through tree as a long as the currentNode has children
+    # this explores the entire branch getting closer to the target value till we reach None
+    while currentNode is not None:
+        # check if currentNode is closer than stored closest. If so, update closest
+        # to current node's value
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        # make use of the BST property to determine what side of any given node has
+        # values close to the target value and is therefore worth exploring
+        # check if target is less than currentNode value. If so update currentNode to
+        # the left node in the branch since this value will be smaller than the value to
+        # the right therefore being possible closer to the target value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        # check if target is greater than curretNode value. If so update currentNode to
+        # the right node in the branch since this value will be greater than the value to
+        # the left therefore being possibly closer to the target value
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
+
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
