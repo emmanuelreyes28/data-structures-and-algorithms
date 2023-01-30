a5d83f09369213ff576681c8648afa0828345e59
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-01-29 19:32:50 -0800

    arrayOfProducts solution

---
+++ arrayOfProducts.py
@@ -0,0 +1,25 @@
+# O(n) time | O(n) space - where n is the length of the input array
+def arrayOfProducts(array):
+    # create determined length arrays with value 1 as placeholder
+    products = [1 for _ in range(len(array))]
+    leftProducts = [1 for _ in range(len(array))]
+    rightProducts = [1 for _ in range(len(array))]
+
+    # get the product of all values to the left of element
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        leftProducts[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+
+    # get the product of all values to the right of element
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        rightProducts[i] = rightRunningProduct
+        rightRunningProduct *= array[i]
+
+    # populate products array with result by multiplying the values at the
+    # given index
+    for i in range(len(array)):
+        products[i] = leftProducts[i] * rightProducts[i]
+
+    return products
