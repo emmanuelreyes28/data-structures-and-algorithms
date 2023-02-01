0cb71a501bd3ff27d72f96f917c103f0a2a3f2a6
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-01-31 22:52:21 -0800

    Add comments to solution

--- firstDuplicateValue.py
+++ firstDuplicateValue.py
@@ -1,3 +1,8 @@
+# O(n) time | O(1) space - where n is the length of the input array
+# iterate through array if we have not yet come across the value turn it into a negative
+# value. Once we come across a negative value we know we have visited this value before
+# therefore it is a duplicate. We are able to do this because the given array is between
+# 1 and n, inclusive, where n is the length of the array.
 def firstDuplicateValue(array):
     for value in array:
         absValue = abs(value)
@@ -5,3 +10,6 @@ def firstDuplicateValue(array):
             return absValue
         array[absValue - 1] *= -1
     return -1
+
+
+print(firstDuplicateValue([2, 1, 5, 3, 3, 2, 4]))
