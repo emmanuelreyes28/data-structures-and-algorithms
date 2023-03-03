3dd503130f6bdd4e9c94450a44fd127be0780ef1
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-03-02 23:24:44 -0800

    bstConstruction contains method

--- bstConstruction.py
+++ bstConstruction.py
@@ -28,3 +28,24 @@ class BST:
                 else:
                     currentNode = currentNode.right
         return self
+
+    # Average: O(log(n)) time | O(1) space
+    # Worst: O(n) time | O(1) space
+    def contains(self, value):
+        # srtore bst object as currentNode
+        currentNode = self
+
+        # keep iterating down the tree up until you reach a leaf node
+        # if None is reached then value does not exist in tree therefore we return False
+        # otherwise return true
+        while currentNode is not None:
+            # if value is less the the currentNode value then search on the left side of tree
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            # if value is greater than the currentNode value then search on the right side of tree
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            # enter default case if value is equal to currentNode value meaning it is contained in the tree
+            else:
+                return True
+        return False
